248b1e63586db84e89829138bd01a4203d24a74d
dev-goutham <dev.gouthamram95@gmail.com>
2023-03-17 18:57:09 +0530

    added footer image

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,6 @@
 const mix = require('laravel-mix');
 
 mix
-  .ts('src/ts/main.ts', 'assets/main.js')
   .ts('src/ts/web-components/drawer.ts', 'assets/component-drawer.js')
   .ts('src/ts/web-components/icon-button.ts', 'assets/component-icon-button.js')
   .sass('src/scss/styles.scss', 'assets/styles.css')
