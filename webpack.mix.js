99a936a594a737488d936c09b86c4c2e6f79ee02
dev-goutham <dev.gouthamram95@gmail.com>
2023-03-26 23:19:21 +0530

    added sort functionality to collection page

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,26 +2,14 @@ const mix = require('laravel-mix');
 
 mix
   .ts('src/ts/pages/product.js', 'assets/product.js')
-  .ts(
-    'src/ts/web-components/drawer.ts',
-    'assets/component-drawer.js',
-  )
-  .ts(
-    'src/ts/web-components/icon-button.ts',
-    'assets/component-icon-button.js',
-  )
+  .ts('src/ts/web-components/drawer.ts', 'assets/component-drawer.js')
+  .ts('src/ts/web-components/icon-button.ts', 'assets/component-icon-button.js')
   .ts(
     'src/ts/web-components/currency-selector.ts',
     'assets/component-currency-selector.js',
   )
-  .ts(
-    'src/ts/web-components/variant.ts',
-    'assets/component-variant.js',
-  )
-  .ts(
-    'src/ts/web-components/cart.ts',
-    'assets/component-cart.js',
-  )
+  .ts('src/ts/web-components/variant.ts', 'assets/component-variant.js')
+  .ts('src/ts/web-components/cart.ts', 'assets/component-cart.js')
   .sass('src/scss/styles.scss', 'assets/styles.css')
   .options({
     postCss: [
